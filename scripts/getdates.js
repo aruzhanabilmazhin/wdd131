dedcea819a354c5e46fdd3eb08d7f5cf917e6332
aruzhana <aruzhana@byui.edu>
2024-11-09 16:32:55 +0500

    ok

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Get last modified date of the document
+document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
